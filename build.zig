5578664515dea074948ba9d2ae333dfeee9a36ec
Maxime <88448038+maxime-linux@users.noreply.github.com>
2025-10-10 00:56:08 +0200

    chore: add zigglgen & zglfw

--- build.zig
+++ build.zig
@@ -1,22 +1,40 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
-        .optimize = b.standardOptimizeOption(.{}),
-        .target = b.standardTargetOptions(.{}),
+        .optimize = optimize,
+        .target = target,
     });
+
+    const gl_bindings = @import("zigglgen").generateBindingsModule(b, .{
+        .api = .gl,
+        .version = .@"4.6",
+        .profile = .core,
+        .extensions = &.{},
+    });
+    exe_mod.addImport("opengl", gl_bindings);
 
     const exe = b.addExecutable(.{
         .name = "scop",
         .root_module = exe_mod,
     });
 
+    const zglfw = b.dependency("zglfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("glfw", zglfw.module("glfw"));
+    exe.linkLibC();
+    exe.linkSystemLibrary("glfw");
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     const run_step = b.step("run", "Run the application");
-
     run_step.dependOn(&run_cmd.step);
 }
