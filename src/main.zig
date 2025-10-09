753e2493650e495b3d18570df2ab5da32d64e559
Maxime <88448038+maxime-linux@users.noreply.github.com>
2025-10-10 00:57:51 +0200

    feat: first window

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,37 @@
 const std = @import("std");
+const gl = @import("opengl");
+const glfw = @import("glfw");
 
-pub fn main() void {
-    std.debug.print("Hello World!", .{});
+const WITDH = 800;
+const HEIGHT = 600;
+
+var procs: gl.ProcTable = undefined;
+
+pub fn main() !void {
+    try glfw.init();
+    defer glfw.terminate();
+
+    glfw.windowHint(glfw.ContextVersionMajor, 4);
+    glfw.windowHint(glfw.ContextVersionMinor, 6);
+    glfw.windowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile);
+
+    const window = try glfw.createWindow(WITDH, HEIGHT, "scop", null, null);
+    defer glfw.destroyWindow(window);
+
+    glfw.makeContextCurrent(window);
+
+    if (!procs.init(glfw.getProcAddress)) return error.InitFailed;
+
+    gl.makeProcTableCurrent(&procs);
+    defer gl.makeProcTableCurrent(null);
+
+    gl.Viewport(0, 0, WITDH, HEIGHT);
+
+    while (!glfw.windowShouldClose(window)) {
+        const alpha: gl.float = 1;
+        gl.ClearColor(1, 1, 1, alpha);
+        gl.Clear(gl.COLOR_BUFFER_BIT);
+        glfw.swapBuffers(window);
+        glfw.pollEvents();
+    }
 }
