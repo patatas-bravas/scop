1eca73662ebb8c210c36b87b6359a55ef6970712
Maxime Collas <88448038+maxime-linux@users.noreply.github.com>
2025-10-13 17:27:21 +0200

    feat: continue parser

--- src/parser.zig
+++ src/parser.zig
@@ -1,28 +1,41 @@
 const std = @import("std");
 const utils = @import("utils.zig");
 
-pub fn ParseObjFile() !void {
+pub const ObjData = struct {
+    v_positions: std.ArrayList([3]f32),
+};
+
+fn getVertexPosition(
+    contents: *SplitIterator,
+    obj_data: *ObjData,
+) !void {
+    var i: usize = 0;
+    var vertices: [3]f32 = undefined;
+    while (contents.next()) |content| : (i += 1) {
+        vertices[i] = std.fmt.parseFloat(f32, content) catch return error.Parsing;
+    }
+    return;
+}
+
+pub fn parseObjFile(path: []const u8) !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const args = try std.process.argsAlloc(allocator);
-    if (args.len < 2) {
-        std.log.err("./scop [PATH OBJ] [PATH TEXTURE]", .{});
-        return error.BadArgs;
-    }
-
     const file = try utils.getRawFile(args[1], allocator);
+
+    var obj_data = RawObj{ .vertexs = std.ArrayList([3]f32).init(allocator) };
 
     var lines = std.mem.splitScalar(u8, file, '\n');
     while (lines.next()) |line| {
-        if (std.mem.eql(u8, line, ""))
+        if (std.mem.eql(u8, line, "") || std.mem.eql(u8, line, "#"))
             continue;
-        const trimmed = std.mem.trim(u8, line, " ");
-        if (contents.next()) |first| {
-            if (std.mem.eql(u8, first, "v")) {} else if (std.mem.eql(u8, first, "vt")) {} else if (std.mem.eql(u8, first, "vn")) {} else if (std.mem.eql(u8, first, "vp")) {} else if (std.mem.eql(u8, first, "f")) {} else if (std.mem.eql(u8, first, "o")) {} else if (std.mem.eql(u8, first, "l")) {} else if (std.mem.eql(u8, first, "mtllib")) {} else if (std.mem.eql(u8, first, "usemtl")) {} else {
-                return error.BadFile;
-            }
+
+        var contents = std.mem.splitScalar(u8, std.mem.trim(u8, line, " "), ' ');
+        if (contents.next()) |data_type| {
+            if (std.mem.eql(u8, data_type, "v")) {
+                try getVertexPosition(&contents, &obj_data);
+            } else {}
         }
     }
 }
