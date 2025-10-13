9677b1c5926991ba5dd395776bcb1e76ddffb4ea
Maxime Collas <88448038+maxime-linux@users.noreply.github.com>
2025-10-13 16:46:42 +0200

    refactor: adopt Zig naming conventions

--- src/zglfw.zig
+++ src/zglfw.zig
@@ -7,40 +7,42 @@ pub const CONTEXT_VERSION_MINOR = c.GLFW_CONTEXT_VERSION_MINOR;
 pub const OPENGL_PROFILE = c.GLFW_OPENGL_PROFILE;
 pub const OPENGL_CORE_PROFILE = c.GLFW_OPENGL_CORE_PROFILE;
 
+pub const Window = c.GLFWwindow;
+
 pub const GetProcAddress = c.glfwGetProcAddress;
 
-pub fn Init() c_int {
+pub fn init() c_int {
     return c.glfwInit();
 }
 
-pub fn WindowHint(hint: c_int, value: c_int) void {
+pub fn windowHint(hint: c_int, value: c_int) void {
     return c.glfwWindowHint(hint, value);
 }
 
-pub fn Terminate() void {
+pub fn terminate() void {
     return c.glfwTerminate();
 }
 
-pub fn CreateWindow(width: c_int, height: c_int, title: [*:0]const u8, monitor: ?*c.GLFWmonitor, share: ?*c.GLFWwindow) ?*c.GLFWwindow {
+pub fn createWindow(width: c_int, height: c_int, title: [*:0]const u8, monitor: ?*c.GLFWmonitor, share: ?*c.GLFWwindow) ?*c.GLFWwindow {
     return c.glfwCreateWindow(width, height, title, monitor, share);
 }
 
-pub fn DestroyWindow(window: *c.GLFWwindow) void {
+pub fn destroyWindow(window: *c.GLFWwindow) void {
     return c.glfwDestroyWindow(window);
 }
 
-pub fn MakeContextCurrent(window: *c.GLFWwindow) void {
+pub fn makeContextCurrent(window: *c.GLFWwindow) void {
     return c.glfwMakeContextCurrent(window);
 }
 
-pub fn WindowShouldClose(window: *c.GLFWwindow) c_int {
+pub fn windowShouldClose(window: *c.GLFWwindow) c_int {
     return c.glfwWindowShouldClose(window);
 }
 
-pub fn SwapBuffers(window: *c.GLFWwindow) void {
+pub fn swapBuffers(window: *c.GLFWwindow) void {
     return c.glfwSwapBuffers(window);
 }
 
-pub fn PollEvents() void {
+pub fn pollEvents() void {
     return c.glfwPollEvents();
 }
